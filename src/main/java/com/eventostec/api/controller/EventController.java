64ed5fc01f748d3016e4ced215bd79d2bdf2bb83
Alef Frade <alef.frade.01tec@gmail.com>
2024-07-23 17:13:47 -0300

    feat: fix endpoint

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/api/event")
 public class EventController {
 
     @Autowired
